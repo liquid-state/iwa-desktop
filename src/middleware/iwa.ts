47f8aac687eb935d711dfd903996202f2b1efcf3
Chris Hopkins <chris@liquid-state.com>
2019-11-01 10:22:38 +1000

    Add online status to app domain

--- src/middleware/iwa.ts
+++ src/middleware/iwa.ts
@@ -11,14 +11,15 @@ const iwaMiddleware: OnSendMiddleware = dispatch => (next, done) => message => {
   switch (message.eventType) {
     case 'http':
       const data = message.data.data as any;
-      fetch(data.url, data)
-        .then(res => dispatch({
-          purpose: "response",
-          eventType: "http",
+      fetch(data.url, data).then(res =>
+        dispatch({
+          purpose: 'response',
+          eventType: 'http',
           request_id: message.data.request_id,
-          response_data: res
-        }));
-      done()
+          response_data: res,
+        })
+      );
+      done();
       break;
     case 'navigate_back':
       window.history.back();
@@ -28,6 +29,6 @@ const iwaMiddleware: OnSendMiddleware = dispatch => (next, done) => message => {
       console.warn('Unhandled iwa domain event', message);
       return next(message);
   }
-}
+};
 
 export default iwaMiddleware;
